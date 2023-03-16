0f81636647d9757c77e47f12244bb57319d9beeb
Luigi De Russis <luigidr@gmail.com>
2023-03-16 11:00:18 +0100

    updated with the latest Dialogflow library

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This repository is an example for getting started with voice user interfaces.
 
 It uses the Web Speech API (experimental) for speech-to-text and text-to-speech operations. For Natural Language Processing, it uses [Dialogflow ES](https://dialogflow.cloud.google.com) agent, included as a `.zip` file.
 
-Tested in Chrome 57+, Firefox 70+, and Safari 10.1+. Please, notice that speech-to-text capability does not work in Safari and Firefox.
+Tested in Chrome 110+, Firefox 109+, and Safari 16+. Please, notice that speech-to-text capability might not work in Safari and Firefox.
 
 To get started with the project, import the `WeatherAgent.zip` into Dialogflow, generate a _service key_ from Google (in JSON), and download it into the root of the project as `client-secret.json`.
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from google.oauth2 import service_account
-import dialogflow_v2 as dialogflow
+from google.cloud import dialogflow
 
 app = Flask(__name__)
 
@@ -22,15 +22,17 @@ def detect_intent():
     session = session_client.session_path("weather-lvbjmw", 123456)
 
     # prepare a new query input with the inner text, taken from the POST request
-    text_input = dialogflow.types.TextInput(text=request.form["message"], language_code="en-US")
-    query_input = dialogflow.types.QueryInput(text=text_input)
+    text_input = dialogflow.TextInput(text=request.form["message"], language_code="en-US")
+    query_input = dialogflow.QueryInput(text=text_input)
 
     # send the request and get the response
     response = session_client.detect_intent(session=session, query_input=query_input)
 
+    # print(response.query_result.intent.displayName)
+    # print(response.query_result.parameters['geo-city'])
+
     # reply to the POST request: the textual response is in "fullfilment"
     return response.query_result.fulfillment_text
-
 
 if __name__ == '__main__':
     app.run()
